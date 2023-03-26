52c7a88756ead39e128ce7e357374a4c36ff223f
kevalrajpalknight <kevalprogram100@gmail.com>
2023-03-26 09:57:09 +0530

    added swagger and integrated configuration setup

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -1,16 +1,13 @@
 """
 WSGI config for project project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/gunicorn/
 """
-
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
 
+from configurations.wsgi import get_wsgi_application  # noqa
 application = get_wsgi_application()
